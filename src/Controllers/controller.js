3687c08e5e552c4b531f9084aa4e0f052f2a9289
Matthuris <175315171+Matthuris@users.noreply.github.com>
2025-05-18 11:30:57 +0100

    Add date-fns and do some not so atomic commits.

--- src/Controllers/controller.js
+++ src/Controllers/controller.js
@@ -1,6 +1,7 @@
 import { model } from "../Models/model";
 import { view } from "../Views/view";
 import "../styles.css";
+import { format } from "date-fns";
 
 const controller = {
   linkObjProp(obj, key, val) {
@@ -35,14 +36,14 @@ const controller = {
     return model.lists[listIndex].items.push(item);
   },
   linkNewItem(name, listIndex = 0) {
-    return this.linkItem(this.createItem(name, listIndex), listIndex);
+    this.linkItem(this.createItem(name, listIndex), listIndex);
   },
 
   linkItemDesc(item, desc) {
     this.linkObjProp(item, "description", desc);
   },
-  linkItemDueDate(item, year, month, day) {
-    this.linkObjProp(item, "dueDate", new Date(year, month - 1, day));
+  linkItemDueDate(item, date) {
+    this.linkObjProp(item, "dueDate", format(new Date(date), "hh:mm dd/MM/yyyy"));
   },
   linkItemPriority(item, priority) {
     this.linkObjProp(item, "priority", priority);
@@ -61,16 +62,27 @@ const controller = {
       item.index = model.lists[listIndex].items.length - 1;
     });
   },
+  getItem(itemIndex, listIndex) {
+    return model.lists[listIndex].items[e.target.getAttribute(itemIndex)];
+  },
 };
 
 const eventListeners = {
+  activeList: 0,
   listForm: view.getElement(".list-form"),
   listInput: view.getElement(".list-input"),
+  itemForm: view.getElement(".item-form"),
+  itemName: view.getElement(".item-name-input"),
+  itemDate: view.getElement(".item-date-input"),
+  itemPriority: view.getElement(".item-priority-input"),
+  itemDesc: view.getElement(".item-desc-textarea"),
+  curName: null,
 
   listenList() {
-    view.addGlobalEventListener("click", "list", (e) => {
+    view.addGlobalEventListener("click", "list-name", (e) => {
+      this.activeList = e.target.getAttribute("index");
       view.deleteChildDivs(view.getElement(".items"));
-      view.displayItems(model.lists[e.target.getAttribute("index")].items);
+      view.displayItems(model.lists[this.activeList].items);
     });
   },
 
@@ -101,9 +113,128 @@ const eventListeners = {
   },
   listenOpenItemForm() {
     view.addGlobalEventListener("click", "btn-item", (e) => {
+      if (model.lists.length > 0) {
+        view.toggleHidden(view.getElement(".btn-item"));
+        view.toggleHidden(view.getElement(".item-form"));
+        view.getElement(".item-name-input").focus();
+      }
+    });
+  },
+  listenAddItem() {
+    view.addGlobalEventListener("submit", "item-form", (e) => {
+      e.preventDefault();
+      if (this.itemName.value === "") {
+        return;
+      }
+      controller.linkNewItem(this.itemName.value, this.activeList);
+
+      controller.linkItemDueDate(
+        controller.readListItem(this.activeList, model.lists[this.activeList].items.length - 1),
+        this.itemDate.value
+      );
+
+      controller.linkItemPriority(
+        controller.readListItem(this.activeList, model.lists[this.activeList].items.length - 1),
+        this.itemPriority.value
+      );
+
+      controller.linkItemDesc(
+        controller.readListItem(this.activeList, model.lists[this.activeList].items.length - 1),
+        this.itemDesc.value
+      );
+
+      this.itemForm.reset();
       view.toggleHidden(view.getElement(".btn-item"));
       view.toggleHidden(view.getElement(".item-form"));
-      view.getElement(".item-name-input").focus();
+      view.deleteChildDivs(view.getElement(".items"));
+      view.displayItems(model.lists[this.activeList].items);
+    });
+  },
+  listenCloseItemForm() {
+    view.addGlobalEventListener("click", "btn-item-input-no", (e) => {
+      view.toggleHidden(this.itemForm);
+      this.itemForm.reset();
+      view.toggleHidden(view.getElement(".btn-item"));
+    });
+  },
+  // listenClickOutForm(form, callback) {
+  //   document.addEventListener("click", (e) => {
+  //     if (!form.contains(e.target)) {
+  //       callback();
+  //     }
+  //   });
+  // },
+  listenOpenItem() {
+    view.addGlobalEventListener("click", "item-name", (e) => {
+      console.log("test");
+      view.displayItemProps(
+        e.target,
+        model.lists[this.activeList].items[e.target.getAttribute("index")]
+      );
+      console.log(e.target);
+      console.log(model.lists[this.activeList].items[e.target.getAttribute("index")]);
+    });
+  },
+  // listenCloseItem(){
+
+  // }
+  listenHover() {
+    view.addGlobalEventListener("mouseover", "list", (e) => {
+      console.log(e.target);
+      let hoveredList = e.target.parentElement;
+      view.toggleHidden(e.target.parentElement.children[1]);
+      // view.addGlobalEventListener("mouseout", "list", (e) => {
+      //   view.toggleHidden(e.target.childNodes[1]);
+      // });
+    });
+  },
+
+  listenDelete() {
+    view.addGlobalEventListener("click", "delete", (e) => {
+      if (e.target.closest(".list")) {
+        controller.deleteList(e.target.closest("[index]").getAttribute("index"));
+        view.deleteChildDivs(view.getElement(".lists"));
+        view.displayLists(model.lists);
+        this.activeList = 0;
+        view.deleteChildDivs(view.getElement(".items"));
+        if (model.lists.length > 0) {
+          view.displayItems(model.lists[0].items);
+        }
+      } else {
+        controller.deleteItem(e.target.closest("[index]").getAttribute("index"), this.activeList);
+        view.deleteChildDivs(view.getElement(".items"));
+        view.displayItems(model.lists[this.activeList].items);
+      }
+    });
+  },
+  listenEdit() {
+    view.addGlobalEventListener("click", "edit", (e) => {
+      if (e.target.closest(".list")) {
+        let targetList = e.target.closest(".list");
+        this.curName = model.lists[targetList.getAttribute("index")].name;
+        view.deleteChildDivs(targetList);
+        view.createListInput(targetList);
+        targetList.firstChild.value = this.curName;
+        targetList.firstChild.focus();
+        view.createSubmitCancel(targetList);
+      }
+    });
+  },
+  listenSubmit() {
+    view.addGlobalEventListener("click", "btn-edit-yes", (e) => {
+      let targetList = e.target.closest(".list");
+      model.lists[targetList.getAttribute("index")].name =
+        targetList.closest(".list").firstChild.value;
+      view.deleteChildDivs(targetList.parentElement);
+      view.displayLists(model.lists);
+      console.log(model.lists);
+    });
+  },
+  listenCancelEdit() {
+    view.addGlobalEventListener("click", "btn-edit-no", (e) => {
+      let listsContainer = e.target.closest(".lists");
+      view.deleteChildDivs(listsContainer);
+      view.displayLists(model.lists);
     });
   },
   listenAll() {
@@ -112,6 +243,15 @@ const eventListeners = {
     this.listenAddList();
     this.listenCloseList();
     this.listenOpenItemForm();
+    this.listenAddItem();
+    this.listenCloseItemForm();
+    // this.listenClickOutForm(this.listForm, () => view.toggleHidden(this.listForm));
+    this.listenOpenItem();
+    // this.listenHover();
+    this.listenDelete();
+    this.listenEdit();
+    this.listenSubmit();
+    this.listenCancelEdit();
   },
 };
 eventListeners.listenAll();
@@ -120,10 +260,16 @@ const demoLists = () => {
   controller.linkNewList("My To-Do List");
   controller.linkNewList("My Wish List");
   controller.linkNewItem("Go upstairs");
+  controller.linkItemDesc(controller.readListItem(0, 0), "...duh");
+  controller.linkItemDueDate(controller.readListItem(0, 0), Date.parse("May 15 2025 12:56:00"));
+  controller.linkItemPriority(controller.readListItem(0, 0), "low");
   controller.linkNewItem("Go downstairs");
+  controller.linkItemDesc(controller.readListItem(0, 1), "...duh");
+  controller.linkItemDueDate(controller.readListItem(0, 1), Date.parse("May 15 2025 12:56:00"));
+  controller.linkItemPriority(controller.readListItem(0, 1), "low");
   controller.linkNewItem("A puppy", 1);
   controller.linkItemDesc(controller.readListItem(1, 0), "Labrador");
-  controller.linkItemDueDate(controller.readListItem(1, 0), 2025, 12, 25);
+  controller.linkItemDueDate(controller.readListItem(1, 0), Date.parse("May 15 2025 12:56:00"));
   controller.linkItemPriority(controller.readListItem(1, 0), "high");
 };
 demoLists();
--- src/Views/view.js
+++ src/Views/view.js
@@ -8,12 +8,27 @@ const view = {
   createDiv() {
     return document.createElement("div");
   },
+  createBtn(text, className, type) {
+    let btn = document.createElement("button");
+    btn.textContent = text;
+    btn.classList.add(className);
+    btn.setAttribute("type", type);
+    return btn;
+  },
+  createInput() {
+    return document.createElement("input");
+  },
 
-  displayName(parent, obj, cls) {
-    parent.appendChild(this.createDiv());
-    parent.lastChild.classList.add(cls);
-    parent.lastChild.textContent = obj.name;
-    parent.lastChild.setAttribute("index", obj.index);
+  displayName(parent, obj, className) {
+    let listContainer = parent.appendChild(this.createDiv());
+    let listName = listContainer.appendChild(this.createDiv());
+    listContainer.classList.add(className);
+    listContainer.setAttribute("index", obj.index);
+    listName.textContent = obj.name;
+    listName.classList.add(`${className}-name`);
+    listName.setAttribute("index", obj.index);
+
+    this.createEditDeleteBtn(parent.lastChild);
   },
 
   getAllLists() {
@@ -45,6 +60,19 @@ const view = {
     });
     displayMessage("");
   },
+  displayItemProp(item, data, property) {
+    item.appendChild(this.createDiv());
+    item.lastChild.textContent = `${data}`;
+    item.lastChild.classList.add(`"item-${property}`);
+  },
+  displayItemProps(item, data) {
+    item.textContent = "";
+    item.classList.add(`${data.priority}`);
+    this.displayItemProp(item, data.name, "name");
+    this.displayItemProp(item, data.dueDate, "date");
+    this.displayItemProp(item, data.priority, "priority");
+    this.displayItemProp(item, data.description, "desc");
+  },
 
   deleteChildDivs(parent) {
     while (parent.firstChild) {
@@ -59,6 +87,31 @@ const view = {
   },
 
   toggleHidden(element) {
+    if (!element) {
+      return;
+    }
     element.classList.toggle("hidden");
+  },
+  createEditDeleteBtn(parent) {
+    parent.addEventListener("mouseenter", function (e) {
+      view.toggleHidden(e.target.querySelector(".edit-delete"));
+    });
+    parent.addEventListener("mouseleave", function (e) {
+      view.toggleHidden(e.target.querySelector(".edit-delete"));
+    });
+    let newDiv = parent.appendChild(this.createDiv());
+    newDiv.classList.add("edit-delete");
+    newDiv.classList.add("hidden");
+    newDiv.appendChild(this.createBtn("✏️", "edit"));
+    newDiv.appendChild(this.createBtn("X", "delete"));
+  },
+  createListInput(parent) {
+    parent.appendChild(this.createInput());
+    parent.lastChild.setAttribute("type", "text");
+  },
+  createSubmitCancel(parent) {
+    let submitCancel = parent.appendChild(this.createDiv());
+    submitCancel.appendChild(this.createBtn("✓", "btn-edit-yes", "submit"));
+    submitCancel.appendChild(this.createBtn("X", "btn-edit-no", "button"));
   },
 };
